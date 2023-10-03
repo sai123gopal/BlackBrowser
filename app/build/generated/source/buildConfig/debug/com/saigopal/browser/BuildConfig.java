0100995bfe21e9964aac13585d08fa1828b13671
sai gopal <sai.gopal@nanohelth@in>
2023-10-03 15:05:29 +0530

    Android 33

--- app/build/generated/source/buildConfig/debug/com/saigopal/browser/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/saigopal/browser/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.saigopal.browser";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 15;
-  public static final String VERSION_NAME = "1.5.23";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "1.5.25";
 }
